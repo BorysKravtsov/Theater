aad63ad4a8f3882535bb51fd29113df2959d4ce4
BorysKravtsov <mamkinboris3@gmail.com>
2024-05-22 02:34:42 +0200

    header and bug fix

--- js/main.js
+++ js/main.js
@@ -1,39 +1,50 @@
-const swiper = new Swiper(".swiper", {
-  effect: "cards",
+const swiper = new Swiper('.swiper', {
+  effect: 'cards',
 
   cardsEffect: {
-    rotate: 0,
+    rotate: 0
   },
   mousewheel: {
-    invert: true,
+    invert: true
   },
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
 });
-const swiperP = new Swiper(".swiperP-container", {
+const swiperP = new Swiper('.swiperP-container', {
   parallax: true,
   centeredSlides: false,
   slidesPerView: 1.64,
+  slidesPerGroup: 1,
   loop: true,
+  loopFillGroupWithBlank: true,
   mousewheel: {
-    invert: true,
-  },
+    invert: true
+  }
 });
-const swiperLogos = new Swiper(".swiperLogos", {
-  slidesPerView: "auto",
+const swiperLogos = new Swiper('.swiperLogos', {
+  slidesPerView: 'auto',
   spaceBetween: 30,
   loop: true,
-  autoplay: true,
+  autoplay: true
 });
 
-document.querySelector(".hamburger").addEventListener("click", function () {
-  document.querySelector(".menu").classList.add("menu-active");
-  document.body.classList.add("lock-scroll");
+document.querySelector('.hamburger').addEventListener('click', function () {
+  document.querySelector('.menu').classList.add('menu-active');
+  document.body.classList.add('lock-scroll');
 });
 
-document.querySelector(".menu__close").addEventListener("click", function () {
-  document.querySelector(".menu").classList.remove("menu-active");
-  document.body.classList.remove("lock-scroll");
+document.querySelector('.menu__close').addEventListener('click', function () {
+  document.querySelector('.menu').classList.remove('menu-active');
+  document.body.classList.remove('lock-scroll');
+});
+
+window.addEventListener('scroll', function () {
+  var header = document.querySelector('.header');
+  if (window.scrollY > 10) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
 });
